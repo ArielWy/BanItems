49b8b8605f1d11f5c66d50bb66b1dce3ccc57974
Ariel <127093985+ariel3007@users.noreply.github.com>
2024-07-14 12:51:36 +0300

    Initial commit - convert to kotlin

---
+++ src/main/java/me/olios/plugins/banitems/BanItems.kt
@@ -0,0 +1,9 @@
+package me.olios.plugins.banitems
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BanItems : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+}
